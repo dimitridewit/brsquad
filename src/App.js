cf0562e1c1ca4ba45fccbdadbd066ec42888d21b
Dimitri de Wit <dimitridewit@outlook.com>
2019-05-25 11:21:55 +0200

    Update homepage and text fix

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
           <h1>BurnoutSquad</h1>
           <p>
             Soon, hopefully not blizzard soon, burnoutsquad comics.
-            A weekly comic about a 2 people and their cat.
+            An irregularly comic about 2 people with a burnout (and their cat).
           </p>
           <p>
             Brought to you with ❤️  by Leo and Dimi.
